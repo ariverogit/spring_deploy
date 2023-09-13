9250918bbb5cc4432a884728cf6b03ce24984efb
ariverogit <anrivero7@gmail.com>
2023-09-13 09:55:43 -0300

    spring heroku app config3

--- src/main/java/com/example/spring_deploy/SpringDeployApplication.java
+++ src/main/java/com/example/spring_deploy/SpringDeployApplication.java
@@ -2,7 +2,6 @@ package com.example.spring_deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringDeployApplication {
 
